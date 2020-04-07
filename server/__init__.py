48259e8c4b387ad6596752e455356b98ad4b9af4
Willow DeKock <willowdekock@gmail.com>
2020-04-06 20:45:04 -0700

    added whitenoise static file serving

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, redirect
 from flask_socketio import SocketIO, send, emit, join_room
+
+from whitenoise import WhiteNoise
 
 import os
 import traceback
 
 def create_app(test_config=None):
-    app = Flask(__name__.split(".")[0])
+    app = Flask(__name__.split(".")[0], static_folder='dist/')
     app.config.from_mapping(
         SECRET_KEY="dev",
         REDIS_DB=("127.0.0.1", 6379, 0) # (host, port, db)
@@ -17,5 +19,12 @@ def create_app(test_config=None):
     else:
         # Load the test config if passed in
         app.config.from_mapping(test_config)
+
+    # Configure whitenoise to serve static files
+    app.wsgi_app = WhiteNoise(app.wsgi_app, root='dist/')
+
+    @app.route('/')
+    def serve_app():
+        return redirect('/index.html')
 
     return app
