36dcb266b5d9f21e760b2118a3ba2dc1746b04b2
Willow DeKock <willowdekock@gmail.com>
2020-04-06 19:49:33 -0700

    added Procfile

--- server/__main__.py
+++ server/__main__.py
@@ -1,6 +1,9 @@
 from . import create_app
 from .api import socketio
+import os
+
+PORT = os.environ.get('PORT', 5000)
 
 app = create_app()
 socketio.init_app(app)
-socketio.run(app, host="0.0.0.0", port="5000", debug=True)
+socketio.run(app, host="0.0.0.0", port=PORT, debug=True)
