663624a5d4b14fa835098854d282f211fa228367
Willow DeKock <willowdekock@gmail.com>
2020-05-07 20:10:14 -0700

    Dynamically generated join URLs + copy to clipboard button

--- src/views/TheMenuNewGame.vue
+++ src/views/TheMenuNewGame.vue
@@ -3,6 +3,7 @@
     <div v-if="joinCode">
         <h2>Game created -- waiting for opponent</h2>
         <h2>Join code: {{joinCode}}</h2>
+        <input type="text" :value="joinUrl" id="joinUrl" readonly><button @click="copyUrl">📋</button>
     </div>
     <div v-else>
         <label>Nickname: <input type="text" v-model="uname"></label>
@@ -22,10 +23,19 @@ export default {
     },
     computed: {
         ...mapState(['joinCode']),
+        joinUrl() {
+            return window.location.origin + this.$router.resolve({path: 'join', query: {code: this.joinCode}}).href;
+        }
     },
     methods: {
         newGame() {
             this.$socket.client.emit('newGame', {nickname: this.uname})
+        },
+        copyUrl() {
+            const input = document.getElementById("joinUrl");
+            input.focus();
+            input.setSelectionRange(0, 999999);
+            document.execCommand("copy");
         }
     }
 }
